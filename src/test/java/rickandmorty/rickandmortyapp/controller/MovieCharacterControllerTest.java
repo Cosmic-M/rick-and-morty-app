5da0c7b273fded98bb10b9c67aa72215ae1f7353
Konstantin Miroshnichenko <bigfishnotbad@gmail.com>
2022-08-29 16:26:46 +0300

    added test for MovieCharacterController

--- src/test/java/rickandmorty/rickandmortyapp/controller/MovieCharacterControllerTest.java
+++ src/test/java/rickandmorty/rickandmortyapp/controller/MovieCharacterControllerTest.java
@@ -1,4 +1,71 @@
-import static org.junit.jupiter.api.Assertions.*;
+package rickandmorty.rickandmortyapp.controller;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import rickandmorty.rickandmortyapp.model.Gender;
+import rickandmorty.rickandmortyapp.model.MovieCharacter;
+import rickandmorty.rickandmortyapp.model.Status;
+import rickandmorty.rickandmortyapp.service.MovieCharacterService;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class MovieCharacterControllerTest {
-  
+    @MockBean
+    private MovieCharacterService movieCharacterService;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        RestAssuredMockMvc.mockMvc(mockMvc);
+    }
+
+    @Test
+    void shouldFindAllCharactersByName() {
+        String name = "Sum";
+        MovieCharacter summerSmith = new MovieCharacter();
+        summerSmith.setName("Summer Smith");
+        summerSmith.setGender(Gender.FEMALE);
+        summerSmith.setStatus(Status.ALIVE);
+        summerSmith.setExternalId(3L);
+        summerSmith.setExternalId(3L);
+
+        MovieCharacter evilSummer = new MovieCharacter();
+        evilSummer.setName("Evil Summer from university C153");
+        evilSummer.setGender(Gender.FEMALE);
+        evilSummer.setStatus(Status.ALIVE);
+        evilSummer.setExternalId(5999L);
+        evilSummer.setExternalId(5999L);
+
+        List<MovieCharacter> mockSummers = List.of(summerSmith, evilSummer);
+
+        Mockito.when(movieCharacterService.findAllByNameContains(name))
+                .thenReturn(mockSummers);
+
+        RestAssuredMockMvc.given()
+                .queryParam("name", name)
+                .when()
+                .get("/movie-characters/by-name")
+                .then()
+                .statusCode(200)
+                .body("size()", Matchers.equalTo(2))
+                .body("[0].name", Matchers.equalTo("Summer Smith"))
+                .body("[0].gender", Matchers.equalTo("FEMALE"))
+                .body("[0].status", Matchers.equalTo("ALIVE"))
+                .body("[1].name", Matchers.equalTo("Evil Summer from university C153"))
+                .body("[1].gender", Matchers.equalTo("FEMALE"))
+                .body("[1].status", Matchers.equalTo("ALIVE"));
+    }
 }
